e6a403d219193babc35a24ac4782f116a018cea8
alexsandro83 <alexsander83@hotmail.com>
2025-09-26 20:37:30 -0300

    projeto limpo react-native

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Olá mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texto: {
+    color: "red",
+    width: "100%",
+    textAlign: "center",
+    fontSize: 40,
+  },
+});
